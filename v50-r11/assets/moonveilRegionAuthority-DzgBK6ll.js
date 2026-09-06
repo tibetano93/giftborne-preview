cfa68d2e2d1b281956165bb92f46f2a5b3d947ef
tibetano93 <tibetano93@gmail.com>
2026-09-06 20:03:24 +0300

    Publish immutable v50-r11 candidate from 647432caaaf7d7dd62efb18b608446e90558c5e2 (add-only)

---
+++ v50-r11/assets/moonveilRegionAuthority-DzgBK6ll.js
@@ -0,0 +1,1 @@
+import{i as e}from"./jsx-runtime-KJkY8l8U.js";import{t}from"./moonveilRegionGeometry-B9gzI8H-.js";import{t as n}from"./regionGeometryAuthority-D5e8-roF.js";var r=e({MOONVEIL_REGION_AUTHORITY:()=>i}),i=n(t);export{r as n,i as t};
